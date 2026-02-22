06dc012b7c56b9c6d6cb4e7ece48a7deb558d4f6
amarjeetprajapati2753 <amarjeet.prajapati@unthinkable.co>
2026-02-22 23:50:09 +0530

    Update myInfo_page.py

--- orange-hrm-automation/pages/myInfo_page.py
+++ orange-hrm-automation/pages/myInfo_page.py
@@ -8,4 +8,9 @@ class MyInfoPage(Page):
     def clickMy_Info(self):
         self.my_info_tab.click()
 
+    def select_nationality_dropdown(self):
+        self.personalDetails_nationality.click()
+        self.personalDetails_nationalitySelect.click()
+        self.personalDetails_saveButton.click()
+
 
